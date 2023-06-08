a7e2371df960243b47c4dde7e6483be662d8210c
Dennis Wanjiru <denniswanjiru71@gmail.com>
2023-06-08 12:57:29 +0300

    isPalindrom

---
+++ isPalindrom/ascii-loop.ts
@@ -0,0 +1,16 @@
+/*** TC: 0(n), SC: O(n) ***/
+function isPalindrome(s: string): boolean {
+  const str = s
+    .toLowerCase()
+    .split("")
+    .filter((l) => {
+      const code = l.charCodeAt(0);
+
+      if ((code > 47 && code < 58) || (code > 96 && code < 123)) return l;
+    })
+    .join("");
+
+  const strReverse = str.split("").reverse().join("");
+
+  return str === strReverse;
+}
